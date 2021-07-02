a35cd9eb2baad359cf4396a92cc64909737ceb54
qarademrel <qarademrel@gmail.com>
2021-07-02 17:07:56 +0400

    vue task

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About Vuetify Todo </h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus aspernatur nobis delectus ea perferendis possimus, ipsa laborum praesentium placeat earum quaerat quam, dolore impedit necessitatibus labore tenetur pariatur, quas asperiores.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur modi, quae perspiciatis sit unde expedita placeat, velit asperiores deleniti consequuntur possimus porro praesentium consectetur quaerat consequatur neque illum, eos fugit?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus aspernatur nobis delectus ea perferendis possimus, ipsa laborum praesentium placeat earum quaerat quam, dolore impedit necessitatibus labore tenetur pariatur, quas asperiores.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur modi, quae perspiciatis sit unde expedita placeat, velit asperiores deleniti consequuntur possimus porro praesentium consectetur quaerat consequatur neque illum, eos fugit?</p>
   </div>
 </template>
